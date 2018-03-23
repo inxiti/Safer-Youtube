1622552a2bf59a67a760cec4ea83bfc9d449865d
inxiti <32788205+inxiti@users.noreply.github.com>
2018-03-23 18:02:17 -0400

    updated

--- src/yhd.js
+++ src/yhd.js
@@ -1,11 +1,4 @@
 @-moz-document domain("youtube.com") {
-	/*
-	UserScript to disable the description, and comments.
-
-	The primary use case is to allow people to use youtube without worrying about what links
-	they click on to prevent falling for: scams, trojans, viruses, untrusted sources, etc.
-	*/
-
 	#description {
 	    pointer-events: none;
 	    display: none;
